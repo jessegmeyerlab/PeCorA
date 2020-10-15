537a1037472c1c0352d2286513ce6ae1497db56e
Maria <maria.dermit@qmul.ac.uk>
2020-10-15 17:50:20 +0100

    Maria changes 3

---
+++ data-raw/import_processed_data_for_PeCorA.R
@@ -0,0 +1,49 @@
+## code to prepare `import_processed_data_for_PeCorA` dataset goes here
+import_processed_data_for_PeCorA <- function(x){
+  mypath <- system.file(
+    "extdata",
+    x,
+    package = "PeCorA"
+  )
+
+  p <- read.delim(
+    mypath,
+    stringsAsFactors = FALSE,
+  )
+  grep(colnames(p), pattern="LFQ")
+  colnames(p)[1:50]
+  idcol = c("Sequence", "Leading.razor.protein")
+  pmelt <-reshape::melt(p,
+               id.vars=idcol,
+               measure.vars=grep(colnames(p), pattern="LFQ"))
+  head(pmelt)
+
+  levels(as.factor(pmelt$variable))
+
+  pmelt$Condition = rep(0, nrow(pmelt))
+  pmelt$Condition[grep(pmelt$variable, pattern="_COVID19")]<-"COVID"
+  head(pmelt)
+  pmelt$Condition[grep(pmelt$variable, pattern="NON.COVID19")]<-"NONCO"
+  pmelt$Condition[grep(pmelt$variable, pattern="control")]<-"CNTRL"
+
+
+
+  levels(as.factor(pmelt$Condition))
+
+  splt_last = function(x){tail(unlist(strsplit(x, "_")), n=1)}
+  charnames<-as.character(pmelt$variable)
+  charnames[1]
+  splt_last(charnames[1])
+  pmelt$BioReplicate<-lapply(FUN=splt_last, charnames)
+  head(pmelt)
+  df <- apply(pmelt,2,as.character)
+  head(df)
+  df[,1]
+  colnames(df)
+  colnames(df)[1]<-"Peptide.Modified.Sequence"
+  colnames(df)[2]<-"Protein"
+  return(df)
+}
+
+#t<-import_processed_data_for_PeCorA("peptides.txt")
+usethis::use_data(import_processed_data_for_PeCorA, overwrite = TRUE)
